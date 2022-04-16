60a9f0a9ccd8db51bece95ffe3933021a35ab59b
shiva2055 <74695215+shiva2055@users.noreply.github.com>
2022-04-15 18:05:39 -0700

    Add files via upload

---
+++ cs380/src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/App.css";
+
+function Home() {
+  return (
+    <>
+      <div className="App">
+        <h1>Online Table reservation</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
